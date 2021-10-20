ef2cf0b6d54cc5c93e3c622d1bb431d525669e39
TOLUWALASE IFEBAJO - STUDENT <G00382657@gmit.ie>
2021-10-20 11:25:38 +0100

    lab4

--- src/components/create.js
+++ src/components/create.js
@@ -1,12 +1,106 @@
 import React, { Component } from 'react';
 
 // this is a new component called create 
-class Create extends Component
-{
-    render(){
-        return(
+class Create extends Component {
+    constructor() {
+        //This is where the handles are binded together
+        super();
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.onChangeMovieName = this.onChangeMovieName.bind(this);
+        this.onChangeMovieYear = this.onChangeMovieYear.bind(this);
+        this.onChangePoster = this.onChangePoster.bind(this);
+        this.state = {
+            Title: '',
+            Year: '',
+            Poster: ''
+        }
+
+
+    }
+
+    //in this section this is where you can add in the year, title and different posters and also add alert that shows the users their updated details
+    handleSubmit(event) {
+        alert('Movie Name: ' + this.state.Title + " " +
+            'Movie Year: ' + this.state.Year + " " +
+            'Movie Poster: ' + this.state.Poster);
+        event.preventDefault();
+
+        this.setState({
+            Title:'',
+            Year:'',
+            Poster:''
+        })
+           
+    }
+    onChangeMovieName(event) {
+        this.setState({
+            Title: event.target.value
+        })
+    }
+
+    onChangeMovieYear(event) {
+        this.setState({
+            Year: event.target.value
+        });
+    }
+
+    onChangePoster(event) {
+        this.setState({
+            Poster: event.target.value
+        });
+
+
+    }
+
+
+
+
+    render() {
+        return (
+            //this is where the buttons and a but of text are added 
             <div>
                 <h2>This is my create component!</h2>
+                <form onSubmit={this.handleSubmit}>
+
+                    <div className="form-group">
+                        <label>Add Movie Name: </label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.Title}
+                            onChange={this.onChangeMovieName}
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Add Movie Year: </label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.Year}
+                            onChange={this.onChangeMovieYear}
+                        />
+
+                    </div>
+
+                    <div className="form-group">
+                        <label>Add Movie Poster: </label>
+                        <textarea type="text"
+                            className="form-control"
+                            value={this.state.Poster}
+                            onChange={this.onChangePoster}
+                        />
+
+                    </div>
+
+                    <div>
+                        <input type="Submit"
+                            value="Add movie"
+                            className="btn btn-primary">
+
+                        </input>
+                    </div>
+
+
+                </form>
             </div>
         );
     }
--- src/components/read.js
+++ src/components/read.js
@@ -1,40 +1,29 @@
 import React, { Component } from 'react';
 import Movies from './movies';
+import axios from 'axios';
 
 //this is where all the images are stored
 class Read extends Component
 {
+    componentDidMount() {
+        axios.get('https://jsonblob.com/api/jsonblob/894944504570986496')
+        .then((response)=>{
+            this.setState({mymovies:response.data.movies})
+        })
+        .catch((error)=>{
+            console.log(error);
+        });
+    }
+
     state = {
-        mymovies: [
-            {
-            "Title": "Avengers: Infinity War",
-            "Year": "2018",
-            "imdbID": "tt4154756",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-            },
-            {
-            "Title": "Captain America: Civil War",
-            "Year": "2016",
-            "imdbID": "tt3498820",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-            },
-            {
-            "Title": "Charlie Wilson's War",
-            "Year": "2007",
-            "imdbID": "tt0472062",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMTgwMDgwMDc4MF5BMl5BanBnXkFtZTYwOTU3MDM4._V1_SX300.jpg"
-            }
-            ]
+        mymovies: []
     };
 
     render(){
         return(
             <div>
                 {/* This is the text that is shown on screen */}
-                <h2>This is my the movies showing !</h2>
+                <h2>This is my read component !</h2>
                 <Movies films={this.state.mymovies}></Movies>
             </div>
         );
